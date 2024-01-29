8df13da9517aaba21446445d7522dd492d9d63ee
Cyenoch <cyenoch@outlook.com>
2024-01-29 17:27:29 +0800

    update

--- src/mappings/block/index.ts
+++ src/mappings/block/index.ts
@@ -10,15 +10,7 @@ export async function handleBlock(block: SubstrateBlock) {
     extrinsics.shift()
     for await (const extrinsic of extrinsics) {
       const index = extrinsics.findIndex(_ => _.hash === extrinsic.hash) + 1
-      logger.info(`
-==================Block: ${block.block.header.number.toNumber()}====================:\n
-SECTION: ${extrinsic.method.section}
-METHOD: ${extrinsic.method.method}
-ARGS: ${JSON.stringify(extrinsic.args, null, 2)}
-DATA: ${JSON.stringify(extrinsic.data)}
-${JSON.stringify(extrinsic.unwrap(), null, 2)}}
-\n=================================================`,
-      )
+      let handled = false
       for await (const { match, handler } of blockHandlers) {
         const matcher = [`${extrinsic.method.section}.${extrinsic.method.method}`, `${extrinsic.method.section}.*`]
         if (matcher.includes(match)) {
@@ -28,11 +20,23 @@ ${JSON.stringify(extrinsic.unwrap(), null, 2)}}
               block,
               index,
             })
+            handled = true
           }
           catch (err) {
             logger.warn(err)
           }
         }
+      }
+      if (!handled) {
+        logger.info(`
+==================Unhandle Block: ${block.block.header.number.toNumber()}====================:\n
+SECTION: ${extrinsic.method.section}
+METHOD: ${extrinsic.method.method}
+ARGS: ${JSON.stringify(extrinsic.args, null, 2)}
+DATA: ${JSON.stringify(extrinsic.data)}
+${JSON.stringify(extrinsic.unwrap(), null, 2)}}
+\n=================================================`,
+        )
       }
     }
   }
