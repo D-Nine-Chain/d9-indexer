7d307fb87040878c52081d3ce29dea97276f4d93
Cyenoch <cyenoch@outlook.com>
2024-03-07 16:49:25 +0800

    fix

--- src/handlers/account.ts
+++ src/handlers/account.ts
@@ -6,24 +6,14 @@ import { ss58Encode } from '../utils'
 
 export async function getAccounts(ctx: ProcessorContext<Store>, addresses: string[], encoded = false) {
   const accounts = await ctx.store.findBy(Account, { id: In(addresses) })
-  const newAccounts: Account[] = []
-
   for await (const address of addresses) {
     if (!accounts.find(account => account.id === address)) {
       const newAccount = new Account({
         id: encoded ? address : ss58Encode(address),
       })
-      newAccounts.push(newAccount)
+      await ctx.store.upsert(newAccount)
+      accounts.push(newAccount)
     }
   }
-
-  try {
-    await ctx.store.upsert(newAccounts)
-  }
-  catch (err) {
-    console.warn('ERROR', err)
-    return await getAccounts(ctx, addresses, encoded)
-  }
-
-  return accounts.concat(newAccounts)
+  return accounts
 }
