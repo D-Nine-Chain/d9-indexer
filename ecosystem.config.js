ca7600cf0db37baee4c42392f145b4ee08d21c95
Cyenoch <jgbingzi@outlook.com>
2024-12-18 15:27:25 +0800

    new api pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,12 @@ module.exports = {
       script: 'sqd',
       args: 'serve:prod',
       log_date_format: 'YYYY-MM-DD HH:mm Z'
+    },
+    {
+      name: 'new-api',
+      script: 'sqd',
+      args: 'api:prod',
+      log_date_format: 'YYYY-MM-DD HH:mm Z'
     }
   ]
 }
